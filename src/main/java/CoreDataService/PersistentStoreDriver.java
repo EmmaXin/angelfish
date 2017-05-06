6cc8f7570464330c049aae7ba833445e0d5947c4
JunYing Yeh <junying_yeh@accton.com>
2017-05-06 09:52:43 +0800

    fix style issues

---
+++ src/main/java/CoreDataService/PersistentStoreDriver.java
@@ -0,0 +1,10 @@
+package CoreDataService;
+
+import java.util.Map;
+
+public interface PersistentStoreDriver {
+    int save(String key, byte[] value, Map<String, String> meta);
+
+    // TODO: change the data type of return value
+    Map.Entry<byte[], Map<String, String>> load(String key);
+}
